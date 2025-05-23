9d7377d6debb6103f896df1084c0cca3eb6db0d0
IzaTomacruz <izamarietomacruz0@gmail.com>
2025-05-23 16:43:46 +0800

    created another class for saving the questions

--- quiz_creator_folder/quiz_creator.py
+++ quiz_creator_folder/quiz_creator.py
@@ -6,7 +6,6 @@ class QuizQuestion:
         self.question = "" # Empty string for question
         self.choices = {} # Dictionary for choices 
         self.answer = "" # Empty string that only requires A, B, C, or D
-        self.quiz_data = []
 
     def input_question(self): # Asks to input question and choices
         self.question = input("Enter a question: ")
@@ -29,5 +28,28 @@ class QuizQuestion:
             "choices": self.choices,
             "answer": self.answer
         }
+    
+class QuizCreator:
+    def __init__(self):
+        self.quiz_data = []
+
+    def confirm_save(self):
+        while True:
+            choice = input("Do you want to save it? (yes or no): ").lower()
+            if choice in ["yes", "y"]:
+                return True
+            elif choice in ["no", "n"]:
+                return False
+            else:
+                print("Invalid input, please enter yes or no.")
+
+    def ask_continue(self):
+        choice = input("Would you like to enter another question? (yes or no): ").lower()
+        return choice in ["yes", "y"]
+
+    def save(self):
+        with open("quiz_creator_folder/create_questions.json", "a") as file:
+            json.dump(self.quiz_data, file, indent=4)
+
     
     
--- quiz_creator_folder/quiz_creator_main.py
+++ quiz_creator_folder/quiz_creator_main.py
@@ -1,8 +1,21 @@
-from quiz_creator import QuizQuestion
+from quiz_creator import QuizQuestion, QuizCreator
 
 print("WELCOME TO THE QUIZ CREATOR!\n")
 print("Please write down your questions, choices, and answers below:\n")
 
-question = QuizQuestion()
-question.input_question()
-question.input_answer()
+create = QuizCreator()
+
+while True:
+    question = QuizQuestion()
+    question.input_question()
+    question.input_answer()
+
+    if create.confirm_save():
+        create.quiz_data.append(question.to_dict())
+        print("Question saved")
+
+    if not create.ask_continue():
+        break
+
+create.save() 
+print("Thank you for using Quiz Creator")
